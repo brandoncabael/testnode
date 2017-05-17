cbb22b791629f0d5b381746e8444570aef765b4a
brandoncabael <brandon.cabael@gmail.com>
2017-05-16 22:58:42 -0600

    This is modified for Charlie's issue

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 
 console.log("My dick is bigger than your's!")
+console.log("Just kidding, I haven't actually seen yours... lol")
+console.log("But when you see this, let's get on rocket league!")
+
 
